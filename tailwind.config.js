f823b3e5853f7198601efc2c06009d07f26a61c1
diaz-L <lediar.a.diaz@gmail.com>
2021-10-02 20:59:30 -0400

    modified tailwind.config.js; added orange color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
+        orange: colors.orange,
         photo_3points_gold: "#c7b07a"
       }
     },
