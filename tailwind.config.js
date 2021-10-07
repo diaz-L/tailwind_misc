3a7b4c67c58d28a3cc6561a32459723596597ce1
diaz-L <lediar.a.diaz@gmail.com>
2021-10-06 22:42:14 -0400

    edited tailwind.config.js to include primary color for code bock; created code block for inspiration_for_code code block

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,8 @@ module.exports = {
         orange: colors.orange,
         photo_3points_gold: "#c7b07a",
         review_of_the_architect_primary: "#bba190",
-        visualization_site_primary: "#b09673"
+        visualization_site_primary: "#b09673",
+        inspiration_for_success_primary: "#bfa1a8"
       }
     },
   },
